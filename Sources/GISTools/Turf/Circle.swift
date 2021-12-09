0fc7a1a8b2b616727212861dde7616cc68f1e29c
Thomas Rasch <thomas.rasch@outdooractive.com>
2021-12-09 14:59:40 +0100

    circle(radius:steps:) for Coordinate3D and Point

--- Sources/GISTools/Turf/Circle.swift
+++ Sources/GISTools/Turf/Circle.swift
@@ -5,6 +5,31 @@ import Foundation
 
 // Ported from https://github.com/Turfjs/turf/tree/master/packages/turf-circle
 
+extension Coordinate3D {
+
+    /// Calculates the circle polygon around a coordinate.
+    ///
+    /// - Parameters:
+    ///    - radius: The radius of the circle, in meters
+    ///    - steps: The number of steps (default 64)
+    public func circle(
+        radius: CLLocationDistance,
+        steps: Int = 64)
+        -> Polygon?
+    {
+        guard radius > 0.0, steps > 1 else { return nil }
+
+        var coordinates: [Coordinate3D] = []
+        for i in 0 ..< steps {
+            coordinates.append(destination(distance: radius, bearing: Double(i) * -360.0 / Double(steps)))
+        }
+        coordinates.append(coordinates[0])
+
+        return Polygon([coordinates])
+    }
+
+}
+
 extension Point {
 
     /// Calculates the circle polygon around a point.
@@ -17,16 +42,7 @@ extension Point {
         steps: Int = 64)
         -> Polygon?
     {
-        guard radius > 0.0, steps > 1 else { return nil }
-
-        let center = self.coordinate
-        var coordinates: [Coordinate3D] = []
-        for i in 0 ..< steps {
-            coordinates.append(center.destination(distance: radius, bearing: Double(i) * -360.0 / Double(steps)))
-        }
-        coordinates.append(coordinates[0])
-
-        return Polygon([coordinates])
+        coordinate.circle(radius: radius, steps: steps)
     }
 
 }
