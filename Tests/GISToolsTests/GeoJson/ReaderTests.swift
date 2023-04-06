c4172d0226ad45f13edd83ad798dca05a7f70754
Thomas Rasch <thomas.rasch@outdooractive.com>
2023-04-06 12:41:57 +0200

    Add GeoJsonReader for convenience

---
+++ Tests/GISToolsTests/GeoJson/ReaderTests.swift
@@ -0,0 +1,42 @@
+@testable import GISTools
+import XCTest
+
+// MARK: - RTreeTests
+
+final class ReaderTests: XCTestCase {
+
+    private let pointJson = """
+    {
+        "type": "Point",
+        "coordinates": [100.0, 0.0],
+        "other": "something else"
+    }
+    """
+
+    func testLoadJson() throws {
+        let point = Point(Coordinate3D(latitude: 0.0, longitude: 100.0))
+        let someGeoJson = try XCTUnwrap(GeoJsonReader.geoJsonFrom(json: point.asJson))
+
+        XCTAssertTrue(someGeoJson.type == .point)
+
+        let castedPoint = try XCTUnwrap(someGeoJson as? Point)
+
+        XCTAssertEqual(castedPoint.asJsonString(prettyPrinted: true), point.asJsonString(prettyPrinted: true))
+    }
+
+    func testLoadString() throws {
+        let someGeoJson = try XCTUnwrap(GeoJsonReader.geoJsonFrom(jsonString: pointJson))
+
+        XCTAssertTrue(someGeoJson.type == .point)
+
+        let point = try XCTUnwrap(Point(jsonString: pointJson))
+        let castedPoint = try XCTUnwrap(someGeoJson as? Point)
+
+        XCTAssertEqual(castedPoint.asJsonString(prettyPrinted: true), point.asJsonString(prettyPrinted: true))
+    }
+
+    static var allTests = [
+        ("testLoadJson", testLoadJson),
+    ]
+
+}
