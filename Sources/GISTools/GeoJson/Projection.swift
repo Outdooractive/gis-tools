a1428683718e4d3cb109ea975f7af5c0838d05c1
Thomas Rasch <thomas.rasch@outdooractive.com>
2024-02-22 13:41:54 +0100

    Codable support for Projection

--- Sources/GISTools/GeoJson/Projection.swift
+++ Sources/GISTools/GeoJson/Projection.swift
@@ -1,25 +1,25 @@
 
 /// Projections that this library can handle (EPSG:3857 and EPSG:4326).
 public enum Projection:
+    Int,
     CustomStringConvertible,
+    Codable,
     Sendable
 {
 
-    /// No SRID (invalid/unknown projection)
-    case noSRID
-    /// EPSG:3857 - web mercator
-    case epsg3857
-    /// EPSG:4326 - geodetic
-    case epsg4326
+    /// No SRID (invalid/unknown projection).
+    case noSRID = 0
+    /// EPSG:3857 - web mercator (https://epsg.io/3857).
+    case epsg3857 = 3857
+    /// EPSG:4326 - geodetic (https://epsg.io/4326).
+    case epsg4326 = 4326
 
     /// Initialize a Projection with a SRID number.
     public init?(srid: Int) {
         switch srid {
         // A placeholder for 'No SRID'
         case 0: self = .noSRID
-        // See https://epsg.io/3857
         case 102_100, 102_113, 900_913, 3587, 3785, 3857, 41001, 54004: self = .epsg3857
-        // See https://epsg.io/4326
         case 4326: self = .epsg4326
         default: return nil
         }
@@ -27,11 +27,7 @@ public enum Projection:
 
     /// The receiver's SRID number.
     public var srid: Int {
-        switch self {
-        case .noSRID: return 0
-        case .epsg3857: return 3857
-        case .epsg4326: return 4326
-        }
+        self.rawValue
     }
 
     /// A human readable description of the receiver.
