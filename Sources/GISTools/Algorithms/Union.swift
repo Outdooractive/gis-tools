bef7ef1cf5fdbffd7df06db42ecce5938f521ace
Thomas Rasch <thomas.rasch@outdooractive.com>
2025-07-24 13:42:17 +0200

    Prepare the union feature

--- Sources/GISTools/Algorithms/Union.swift
+++ Sources/GISTools/Algorithms/Union.swift
@@ -3,7 +3,34 @@ import CoreLocation
 #endif
 import Foundation
 
-// Ported from https://github.com/Turfjs/turf/tree/master/packages/turf-union
-// and https://github.com/mfogel/polygon-clipping
+extension Polygon {
 
-// TODO
+    public func union(_ other: PolygonGeometry) -> MultiPolygon {
+        UnionHelper.union(polygons: [self, other])
+    }
+
+}
+
+extension MultiPolygon {
+
+    public func union(_ other: PolygonGeometry) -> MultiPolygon {
+        UnionHelper.union(polygons: [self, other])
+    }
+
+    public mutating func formUnion(_ other: PolygonGeometry) {
+        self = union(other)
+    }
+
+}
+
+struct UnionHelper {
+
+    static func union(polygons: [PolygonGeometry]) -> MultiPolygon {
+        assert(polygons.isNotEmpty, "Input polygons must not be empty")
+
+        let inputPolygons = polygons.map(\.polygons).flatMap({ $0 })
+
+        return MultiPolygon(inputPolygons)!
+    }
+
+}
